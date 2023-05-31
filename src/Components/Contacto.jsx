6ba8bc5325eee7ab5f96e01865b70fc2d7849f70
LeanKrn <leandrotomask@gmail.com>
2023-05-31 16:15:00 -0300

    Update 31/05

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,7 +1,42 @@
 export const Contacto = () => {
   return (
-    <div>
-      <div></div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        border: "1px solid white",
+        margin: "0.777%",
+        height: "20rem",
+        padding: "1%",
+      }}
+    >
+      <div style={{ margin: "auto" }}>
+        <h3 style={{ letterSpacing: "5px" }}>
+          No dudes en contactarme ante cualquier duda o Recomendacion
+        </h3>
+      </div>
+      <div style={{ display: "Flex", justifyContent: "space-around" }}>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/leankrn/"
+            target="_blank"
+            style={{ color: "white", fontSize: "2.2rem", letterSpacing: "3px" }}
+          >
+            Likedind
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="https://wa.me/+5522992282624"
+            target="_blank"
+            style={{ color: "white", fontSize: "2.2rem", letterSpacing: "3px" }}
+          >
+            Whatsapp
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -3,39 +3,57 @@ import "../Styles/Proyecto.css";
 export const Proyectos = () => {
   const info = [
     {
-      Title: "Eccomerse",
-      Text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      Repository: "",
+      Title: "Keran´s food",
+      Text: "App diseñada y desarrollada para dar informacion de recetas que incluye:Busqueda,Filtros,Ordenamientos,Base de datos, Consultas a una api externa etc. El front se compone de React,Redux y style components,",
+      Front: ["React", "Redux", "Style Components"],
+      Back: ["NodeJS", "ExpressJS"],
+      Database: ["PostgresSQL", "Sequelize"],
+      Repository: "https://github.com/LeanKrn/PI-Food-main",
       Demo: "",
-      image: "",
+      image: [
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150920_ntatwj.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150937_vmxhep.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150951_riwwhh.png",
+      ],
     },
     {
-      Title: "Eccomerse",
-      Text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      Title: "Rick And Morty",
+      Text: "App diseñada y desarrollada para dar informacion en forma de cards sobre los personajes de rick and morty:Busqueda, Consultas a una api externa etc. El front se compone de React,Redux y style components",
+      Front: ["React", "Redux", "Style Components"],
+
       Repository: "",
       Demo: "",
-      image: "",
+      image: [
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150920_ntatwj.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150937_vmxhep.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150951_riwwhh.png",
+      ],
     },
     {
-      Title: "Eccomerse",
-      Text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      Title: "Computer Store",
+      Text: "App diseñada y desarrollada sobre un eccomerse de componentes de tecnologia, Trabajo en grupo donde las tecnoligas implementadas mas interesantes son:Authenticacion, Busqueda, Filtros, Ordenamiento, Baneo de usuarios y publicaciones, Email de recuperacion de contraseña y Compra confirmada con el ticket del usuario, Rutas de administrador donde se controlan publicaciones y usuarios, Etc.",
+      Front: ["React", "Redux", "Style Components"],
+      Back: ["NodeJS", "ExpressJS", "Cloudinary", "Mercado pago api", "Auth"],
+      Database: ["PostgresSQL", "Sequelize"],
       Repository: "",
-      Demo: "",
-      image: "",
+      Demo: "https://compstore.netlify.app/",
+      image: [
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150920_ntatwj.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150937_vmxhep.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150951_riwwhh.png",
+      ],
     },
     {
-      Title: "Eccomerse",
-      Text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      Repository: "",
+      Title: "Pokedex Mobile",
+      Text: "App de react native diseñada para mostrar informacion sobre pokemones,donde lo mas interesante es la Authenticacion,Guardado de favoritos y paginado infinito",
+      Front: ["React native", "Redux", "Style Components"],
+      Repository: "https://github.com/LeanKrn/PokedexKRN",
       Demo: "",
-      image: "",
-    },
-    {
-      Title: "Eccomerse",
-      Text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      Repository: "",
-      Demo: "",
-      image: "",
+      image: [
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150920_ntatwj.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150937_vmxhep.png",
+        "https://res.cloudinary.com/dtfppvicy/image/upload/v1685556628/Captura_de_pantalla_2023-05-31_150951_riwwhh.png",
+      ],
     },
   ];
   return (
@@ -44,16 +62,48 @@ export const Proyectos = () => {
       <div className="ProyectDiv">
         {info.map((Proyect) => (
           <div className="InfoProyectDiv">
-            <div className="ImagenProvisoria"></div>
-            <h3 className="TitleInfoProyect">{Proyect.Title}</h3>
-            <p className="TextInfoProyect">{Proyect.Text}</p>
-            <div className="DivLinksProyect">
-              <a href={Proyect.Repository} target="_blank" rel="noreferrer">
-                Respository
-              </a>
-              <a href={Proyect.Demo} target="_blank" rel="noreferrer">
-                Demo
-              </a>
+            <div className="ImagenProvisoria">
+              {Proyect.image.map((img) => (
+                <img src={img} />
+              ))}
+            </div>
+            <div className="DivHidden">
+              <h3 className="TitleInfoProyect">{Proyect.Title}</h3>
+              <p className="TextInfoProyect">{Proyect.Text}</p>
+              <h3>Front-End</h3>
+              <div className="TecFrontDiv">
+                {Proyect.Front.map((Tec) => (
+                  <p className="FrontTec"> {Tec} </p>
+                ))}
+              </div>
+              {Proyect.Back ? (
+                <>
+                  <h3>Back-End</h3>
+                  <div className="TecFrontDiv">
+                    {Proyect.Back.map((Tec) => (
+                      <p className="FrontTec"> {Tec} </p>
+                    ))}
+                  </div>
+                </>
+              ) : null}
+              {Proyect.Database ? (
+                <>
+                  <h4>Data Base</h4>
+                  <div className="TecFrontDiv">
+                    {Proyect.Database.map((Tec) => (
+                      <p className="FrontTec"> {Tec} </p>
+                    ))}
+                  </div>
+                </>
+              ) : null}
+              <div className="DivLinksProyect">
+                <a href={Proyect.Repository} target="_blank" rel="noreferrer">
+                  Respository
+                </a>
+                <a href={Proyect.Demo} target="_blank" rel="noreferrer">
+                  Demo
+                </a>
+              </div>
             </div>
           </div>
         ))}
